b9d4954de163d6dac1a33b0cdd7c576d6269df19
start.vaadin.com <no-reply@vaadin.com>
2022-05-18 10:29:42 +0000

    Generated project

---
+++ src/main/java/be/intecbrussel/bbjja/data/Role.java
@@ -0,0 +1,5 @@
+package be.intecbrussel.bbjja.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/be/intecbrussel/bbjja/views/slides/SlidesView.java
@@ -0,0 +1,33 @@
+package be.intecbrussel.bbjja.views.slides;
+
+import be.intecbrussel.bbjja.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import javax.annotation.security.RolesAllowed;
+
+@PageTitle("Slides")
+@Route(value = "slides", layout = MainLayout.class)
+@RolesAllowed("ADMIN")
+public class SlidesView extends VerticalLayout {
+
+    public SlidesView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
